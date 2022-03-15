2c9fc862d1f13985c1ce924f8902f09fdb9ad37a
lucasmelin <mell@bank-banque-canada.ca>
2022-03-15 19:48:55 +0000

    Fix incorrect test

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,7 +14,7 @@ def test_increase_patch(old_version, expected_version):
 
 @pytest.mark.parametrize(
     "old_version,expected_version",
-    [("0.1.0", "0.2.0"), ("1.0.0", "1.2.0"), ("0.0.1", "0.1.0"), ("0.1.1", "0.2.0")],
+    [("0.1.0", "0.2.0"), ("1.0.0", "1.1.0"), ("0.0.1", "0.1.0"), ("0.1.1", "0.2.0")],
 )
 def test_increase_minor(old_version, expected_version):
     version = increase_version_number(old_version, "minor")
